bb025903f138ff403aa27d5765f5d0c1974a5f73
kirubelatk <kirubelateka@gmail.com>
2024-08-13 23:33:56 +0200

    learned to split the config file for efficiency

--- src/index.js
+++ src/index.js
@@ -28,3 +28,7 @@ contact.addEventListener('click', ()=>{
     content.innerText = '';
     appear3()
 })
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+  }
